59859528ce23df516a8ed6cd81d2b3ffbdf3239f
Harsh Atwal <harshatwal149@gmail.com>
2025-08-28 12:56:02 +0530

    this is my first commit

---
+++ id.js
@@ -0,0 +1,25 @@
+id
+roll_number
+name
+address
+city
+pincode
+course
+
+
+[{
+    id:2,
+    roll_number:"23232",
+    name:"Harsh Kumar",
+    address:"Sidhbari",
+    city:"Dharmshala",
+},
+
+{
+    id:3,
+    roll_number:"23323",
+    name:"Sachin Kumar",
+    address:"Palampur",
+    city:"Kangra",
+}
+]
